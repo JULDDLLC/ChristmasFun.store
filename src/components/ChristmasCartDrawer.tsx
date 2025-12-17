ca09e2c028708dc1f22f216780f0c020d338d185
Julie Todd <julie@juldd.com>
2025-12-16 20:07:28 -0600

    Updated ChristmasCartDrawer.tsx

--- src/components/ChristmasCartDrawer.tsx
+++ src/components/ChristmasCartDrawer.tsx
@@ -1,10 +1,5 @@
 import CartDrawer from "./CartDrawer";
 
-// Named export (fixes: import { ChristmasCartDrawer } from ".../ChristmasCartDrawer")
 export const ChristmasCartDrawer = CartDrawer;
-
-// Default export (fixes: import ChristmasCartDrawer from ".../ChristmasCartDrawer")
 export default CartDrawer;
-
-// Re-export any types/named exports from CartDrawer (optional but helpful)
 export * from "./CartDrawer";
