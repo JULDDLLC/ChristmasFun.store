697cf26afc4ca14e6732e375dbb92db66cc6d78d
Julie Todd <julie@juldd.com>
2025-12-16 19:30:34 -0600

    Updated ChristmasCartDrawer.tsx

--- src/components/ChristmasCartDrawer.tsx
+++ src/components/ChristmasCartDrawer.tsx
@@ -1,3 +1,9 @@
-import CartDrawer
+import CartDrawer from "./CartDrawer";
+
+// IMPORTANT: some files import as a named export { ChristmasCartDrawer }
+export const ChristmasCartDrawer = CartDrawer;
+
+export default CartDrawer;
+
 
 
