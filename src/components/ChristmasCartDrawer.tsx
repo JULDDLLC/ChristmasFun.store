4bd926e1cbceefb7dd2ecccd456f67465127aaa9
Julie Todd <julie@juldd.com>
2025-12-18 10:27:08 -0600

    Updated ChristmasCartDrawer.tsx

--- src/components/ChristmasCartDrawer.tsx
+++ src/components/ChristmasCartDrawer.tsx
@@ -1,5 +1,13 @@
 import CartDrawer from "./CartDrawer";
 
+/**
+ * IMPORTANT:
+ * This component is imported BOTH ways across the app:
+ * 1) import ChristmasCartDrawer from ...
+ * 2) import { ChristmasCartDrawer } from ...
+ *
+ * So we export BOTH.
+ */
+
 export const ChristmasCartDrawer = CartDrawer;
 export default CartDrawer;
-export * from "./CartDrawer";
